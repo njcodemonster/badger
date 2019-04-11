fc6f5a66a4f775e7d9cb02285a7ea5ce70c9e541
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-04-11 19:56:18 +0500

    Add project files.

---
+++ badger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace badger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
