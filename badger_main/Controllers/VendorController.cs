cd65fd90c65bc663649642cc594b5eb308216592
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-05-14 20:58:04 +0500

    vendor controler work stated

---
+++ badger_main/Controllers/VendorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using badgerApi.Interfaces;
+using badgerApi.Models;
+namespace badgerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly IVendorRepository _VendorRepo;
+
+        public VendorController(IVendorRepository VendorRepo)
+        {
+            _VendorRepo = VendorRepo;
+        }
+        // GET: api/Vendor
+        [HttpGet]
+        public async Task<ActionResult<List<Vendor>>> GetAsync()
+        {
+            return await _VendorRepo.GetAll(0);
+        }
+
+        // GET: api/Vendor/5
+        [HttpGet("{id}")]
+        public async Task<Vendor> GetAsync(int id)
+        {
+            return await _VendorRepo.GetByID(id);
+        }
+
+        // POST: api/Vendor
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Vendor/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ badger_main/Interfaces/VendorRepo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using badgerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace badgerApi.Interfaces
+{ 
+    public interface IVendorRepository
+    {
+        Task<Vendor> GetByID(int id);
+        Task<List<Vendor>> GetAll(Int32 Limit);
+    }
+    public class VendorRepo : IVendorRepository
+    {
+        private readonly IConfiguration _config;
+
+        public VendorRepo(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new MySqlConnection(_config.GetConnectionString("ProductsDatabase"));
+            }
+        }
+      
+
+        public async Task<List<Vendor>> GetAll(Int32 Limit)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                conn.Open();
+                IEnumerable<Vendor> result = new List<Vendor>();
+                if(Limit > 0)
+                {
+                    result = await conn.QueryAsync<Vendor>("Select * from vendor Limit 30;");
+                }
+                else
+                {
+                    result = await conn.GetAllAsync<Vendor>();
+                }
+                return result.ToList();
+            }
+        }
+
+       
+
+        public async Task<Vendor> GetByID(int id)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                
+                conn.Open();
+                var result = await conn.GetAsync<Vendor>(id);
+                return result;
+            }
+        }
+    }
+}
+
