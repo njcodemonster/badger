4b2946180e23306e957f342859e78950cba4d695
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-05-25 19:54:38 +0500

    fixed some other stuff

---
+++ badger_main/Interfaces/EventsRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace badgerApi.Interfaces
+{
+    public interface IEventRepo
+    {
+        Task<String> AddEvent<T>(T data, String tableName);
+    }
+    public class EventsRepo : IEventRepo
+    {
+        public Task<string> AddEvent<T>(T data, string tableName)
+        {
+            return String;
+        }
+    }
+}
