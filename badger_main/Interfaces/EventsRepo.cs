3a204453217cdf7fcb4a3b827df1e3eefc354c9c
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-05-25 19:55:30 +0500

    error fix

--- badger_main/Interfaces/EventsRepo.cs
+++ badger_main/Interfaces/EventsRepo.cs
@@ -7,13 +7,13 @@ namespace badgerApi.Interfaces
 {
     public interface IEventRepo
     {
-        Task<String> AddEvent<T>(T data, String tableName);
+        Task<String> AddEventAsync<T>(T data, String tableName);
     }
     public class EventsRepo : IEventRepo
     {
-        public Task<string> AddEvent<T>(T data, string tableName)
+        public async Task<string> AddEventAsync<T>(T data, string tableName)
         {
-            return String;
+            return  "ssdsd";
         }
     }
 }
