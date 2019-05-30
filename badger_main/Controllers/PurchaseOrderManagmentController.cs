5f261e7c009f1087ea6bdc6b31b1ef5018e16dd6
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-05-31 00:05:59 +0500

    PO managment page first dynamic is ready

--- badger_main/Controllers/PurchaseOrderManagmentController.cs
+++ badger_main/Controllers/PurchaseOrderManagmentController.cs
@@ -13,19 +13,35 @@ namespace badgerApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PurchaseOrderManagmentController : ControllerBase
+    public class PurchaseOrderManagementController : ControllerBase
     {
         private readonly IConfiguration _config;
         private readonly IPurchaseOrdersRepository _PurchaseOrdersRepo;
         ILoggerFactory _loggerFactory;
         private INotesAndDocHelper _NotesAndDoc;
         private int note_type = 4;
-        public PurchaseOrderManagmentController(IPurchaseOrdersRepository PurchaseOrdersRepo, ILoggerFactory loggerFactory, INotesAndDocHelper NotesAndDoc, IConfiguration config)
+        public PurchaseOrderManagementController(IPurchaseOrdersRepository PurchaseOrdersRepo, ILoggerFactory loggerFactory, INotesAndDocHelper NotesAndDoc, IConfiguration config)
         {
             _config = config;
             _PurchaseOrdersRepo = PurchaseOrdersRepo;
             _loggerFactory = loggerFactory;
             _NotesAndDoc = NotesAndDoc;
+        }
+
+        [HttpGet("GetLineItemDetails/{PO_id}/{limit}")]
+        public async Task<object> GetLineItemsDetails(int PO_id, int limit)
+        {
+            dynamic LineITemsDetails = new object();
+            try
+            {
+                LineITemsDetails = await _PurchaseOrdersRepo.GetOpenPOLineItemDetails(PO_id,limit);
+            }
+            catch (Exception ex)
+            {
+                var logger = _loggerFactory.CreateLogger("internal_error_log");
+                logger.LogInformation("Problem happened in selecting the data for GetLineItemsDetails for PO : "+PO_id.ToString()+" with message" + ex.Message);
+            }
+            return LineITemsDetails;
         }
 
 
