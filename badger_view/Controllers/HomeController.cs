80de1329cc8bbe0ab3585103e2199135f1a739b9
ubaid workstation <ubaid@7ctch.com>
2019-06-04 17:35:52 +0500

    extra work on form

--- badger_view/Controllers/HomeController.cs
+++ badger_view/Controllers/HomeController.cs
@@ -21,21 +21,21 @@ namespace badger_view.Controllers
         [Authorize]
         public async Task<IActionResult> Index()
         {
-           // Int32? test =  HttpContext.Session.GetInt32("isLogin");
-           // if ( await _LoginHelper.CheckLogin())
-           // {
-                return View();
-           // }
-           // else
-          //  {
-          //      return View("Login");
-           // }
+            // Int32? test =  HttpContext.Session.GetInt32("isLogin");
+            // if ( await _LoginHelper.CheckLogin())
+            // {
+            return View();
+            // }
+            // else
+            //  {
+            //      return View("Login");
+            // }
         }
         [HttpPost("Dologin")]
         public async Task<IActionResult> DoLogin()
         {
 
-            if (await _LoginHelper.DoLogin("x","y"))
+            if (await _LoginHelper.DoLogin("x", "y"))
             {
                 return View("index");
             }
@@ -67,6 +67,11 @@ namespace badger_view.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Form()
+        {
+            return View();
         }
     }
 }
