9481f9b6bb8384687d1fd78be62608f645d8f79d
njcodemonster <ch.naveed.jawaid@gmail.com>
2019-06-29 17:48:25 +0500

    working on tags

---
+++ badger_view/wwwroot/js/productDetails.js
@@ -0,0 +1,44 @@
+﻿var color_added = new Array();
+var color_removed = new Array();
+$(document).on("change", ".colorItem", function () {
+   
+    var color_value = $(this).data("colorvalue");
+    if ($(this).data("current") == "1") {
+        if (this.checked) {
+            if (color_removed.find(item => item === color_value)) {
+                color_removed = color_removed.filter(item => item !== color_value)
+            }
+            else {
+               
+            }
+        }
+        else {
+            if (color_removed.find(item => item === color_value )) {
+                
+            }
+            else {
+                color_removed.push(color_value);
+            }
+        }
+    }
+    else {
+        if (this.checked) {
+            if (color_added.find(item => item === color_value)) {
+                
+            }
+            else {
+                color_added.push(color_value);
+            }
+        }
+        else {
+            if (color_added.find(item => item === color_value)) {
+                color_added = color_added.filter(item => item !== color_value)
+            }
+            else {
+
+            }
+        }
+    }
+    console.log(color_removed);
+    console.log(color_added);
+})
