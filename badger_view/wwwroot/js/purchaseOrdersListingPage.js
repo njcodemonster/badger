9cd6cb8485bf980ede3214f881d76fa9b04929ab
azeem <azeem.7ctech@gmail.com>
2019-07-03 17:32:42 +0500

    chages sigl order

--- badger_view/wwwroot/js/purchaseOrdersListingPage.js
+++ badger_view/wwwroot/js/purchaseOrdersListingPage.js
@@ -361,7 +361,7 @@ $(document).on('click', "#EditPurchaseOrderButton", function () {
     var id = $("#newPurchaseOrderForm").data("currentid");
 
     var jsonData = {};
-
+    $('.poAlertMsg').append('<div class="spinner-border text-info"></div>');
     var delieveryRange = $("#newPurchaseOrderForm #poDelieveryRange").val();
     delieveryRange = delieveryRange.split("-");
 
@@ -781,6 +781,7 @@ function purchaseOrderData(data) {
 }
 function getSinglePurchaseOrder(id) {
     $('.orderNumber').text(id)
+    $("#newPurchaseOrderForm").attr('data-currentid',id)
     $.ajax({
         url: '/purchaseorders/details/' + id,
         dataType: 'json',
